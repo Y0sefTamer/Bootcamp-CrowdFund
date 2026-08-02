a8dc7760fbf72874ae6ef031660d6a951d7c2368
Y0sefTamer <programmeryosef@gmail.com>
2026-08-02 14:07:48 +0300

    feat: Add campaign struct

--- src/CrowdFund.sol
+++ src/CrowdFund.sol
@@ -1,4 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
-contract CrowdFund {}
+contract CrowdFund {
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target; // the goal amount
+        uint256 deadline; // timestamp of the deadline
+        uint256 amountCollected; //pledge amount
+        bool claimed; // whether the campaign has been claimed
+        mapping(address => uint256) donations; // mapping of donors to their donation amounts
+    }
+}
