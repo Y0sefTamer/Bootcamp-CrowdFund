64ce596a4c43ef929d95cca047036a87f413802a
Y0sefTamer <programmeryosef@gmail.com>
2026-08-03 08:51:46 +0300

    feat: Add IERC interface , state variable and constructor
    -IERC20 allow us to use transfer and transfer function without import the whole ERC20
    -this make the ppl to put money with ERC20 token

--- src/CrowdFund.sol
+++ src/CrowdFund.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
-
+interface IERC20 {
+    function transfer(address, uint256) external returns (bool);
+    function transferFrom(address, address, uint256) external returns (bool);
+}
 contract CrowdFund {
     struct Campaign {
         address owner;
@@ -11,10 +14,15 @@ contract CrowdFund {
         uint256 amountCollected; //pledge amount
         bool claimed; // whether the campaign has been claimed
     }
+    IERC20 public immutable token; // ERC20 token used for pledging
 
     uint256 public numberOfCampaigns ; // Total number of campaigns created and id
     // state variable to store all campaigns
     mapping(uint256 => Campaign) public campaigns;
     mapping(uint256 => mapping(address => uint256)) public pledgeAmount; // mapping of campaignId to donor address to pledge amount
+
+    constructor(address _token) {
+        token = IERC20(_token);
+    }
 
 }
